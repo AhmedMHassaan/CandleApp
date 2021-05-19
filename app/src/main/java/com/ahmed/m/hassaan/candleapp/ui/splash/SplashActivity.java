760a367fba9d58b201df1f4055b260b47b4919f0
Ahmed M. Hassaan <aabojana09@gmail.com>
2021-05-19 18:43:36 +0200

    After Adding Packages

--- app/src/main/java/com/ahmed/m/hassaan/candleapp/ui/splash/SplashActivity.java
+++ app/src/main/java/com/ahmed/m/hassaan/candleapp/ui/splash/SplashActivity.java
@@ -1,14 +1,16 @@
-package com.ahmed.m.hassaan.candleapp;
+package com.ahmed.m.hassaan.candleapp.ui.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ahmed.m.hassaan.candleapp.R;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
